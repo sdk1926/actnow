2bda7bd6e1bd31b7296c57575c518ee7a4f37acf
sdk <sdk1926@naver.com>
2021-11-01 16:42:42 +0900

    Initial commit

---
+++ src/test/java/com/sdk/actnow/ActnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdk.actnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
